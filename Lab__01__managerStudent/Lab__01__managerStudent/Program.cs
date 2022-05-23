d395466cc83b16c08671fcce911b7a640e86f55b
Phạm Văn Thiên <80672239+VanThienPham@users.noreply.github.com>
2022-05-23 21:13:43 +0700

    Lab 01

---
+++ Lab__01__managerStudent/Lab__01__managerStudent/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab__01__managerStudent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so luong sinh vien: ");
+            int totalStudent = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+            NhapSinhVien(ref students, totalStudent);
+            XuatSinhVien(students);
+            Console.ReadKey();
+        }
+
+        public static void NhapSinhVien(ref List<Student> students, int n)
+        {
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("---------sinh vien " +
+                    ""+(i+1)+" ---------");
+                Console.Write("Nhap ten sinh vien ");
+                string name_260 = Console.ReadLine();
+                string id_260 = Console.ReadLine();
+                string khoa_260 = Console.ReadLine();
+                double dtb_260 = double.Parse(Console.ReadLine());
+                Student student_260 = new Student(id_260, name_260, khoa_260, dtb_260);
+                students.Add(student_260);
+            }
+        }
+        public static void XuatSinhVien(List<Student> students)
+        {
+            for(int i = 0; i < students.Count; i++)
+            {
+                Console.WriteLine(students[0].export(i + 1));
+            }
+        }
+    }
+}
---
+++ Lab__01__managerStudent/Lab__01__managerStudent/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab__01__managerStudent
+{
+    internal class Student
+    {
+        private string SDI_260;
+        private string ten_260;
+        private string khoa_260;
+        private double dtb_260;
+
+        public Student(string sDI_260, string ten_260, string khoa_260, double dtb_260)
+        {
+            SDI_260 = sDI_260;
+            this.ten_260 = ten_260;
+            this.khoa_260 = khoa_260;
+            this.dtb_260 = dtb_260;
+        }
+
+        public string SDI_2601 { get => SDI_260; set => SDI_260 = value; }
+        public string Ten_260 { get => ten_260; set => ten_260 = value; }
+        public string Khoa_260 { get => khoa_260; set => khoa_260 = value; }
+        public double Dtb_260 { get => dtb_260; set => dtb_260 = value; }
+
+        public string export(int index)
+        {
+            return "Sinh vien "+index + ": \n { ma sinh vien : "+this.SDI_260 +
+                        " ten sinh vien" +this.ten_260 +
+                        " Khoa : " + this.khoa_260 +
+                        " tbl : " + this.dtb_260  ;
+        }
+    }
+}
